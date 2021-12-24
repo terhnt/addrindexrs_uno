c328620a7492fbf58b55733e75441ec7ccce155b
Andrew <95168952+terhnt@users.noreply.github.com>
2021-12-24 16:23:24 +1100

    Change ports to default UnobtaniumCore ports

--- src/config.rs
+++ src/config.rs
@@ -189,9 +189,9 @@ impl Config {
         config.db_dir.push(db_subdir);
 
         let default_daemon_port = match config.network {
-            Network::Bitcoin => 8120,
-            Network::Testnet => 18120,
-            Network::Regtest => 28120,
+            Network::Bitcoin => 65535,
+            Network::Testnet => 65531,
+            Network::Regtest => 18445,
         };
 
         let default_indexer_port = match config.network {
